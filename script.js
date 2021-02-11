8741f0ce55c089e5898ec5363be8b87543f29977
Adedoyin <creativitytraz22@gmail.com>
2021-02-11 19:00:08 +0100

    Added new songs

--- script.js
+++ script.js
@@ -15,10 +15,105 @@ const musicArray = [
 
     {
         artistName: 'westlife',
-        musicFile: '/Westlife_-_My_Love.mp3',
+        musicFile: 'Westlife_-_My_Love.mp3',
         musicName: 'My Love',
         artistPix: 'westlife.jpg'
-    }
+    },
+    
+    {
+        artistName: 'john Legend',
+        musicFile: 'John-Legend-All-Of-Me-via-Naijafinix.com_.mp3',
+        musicName: 'All of Me',
+        artistPix: 'John.jpg'
+    },
+
+    {
+        artistName: 'bruno Mars',
+        musicFile: 'Bruno_Mars_-_Just_The_Way_You_Are_Qoret.com.mp3',
+        musicName: 'Marry You',
+        artistPix: 'bruno.jpg'
+    },
+
+    {
+        artistName: 'timi Dakolo',
+        musicFile: 'Timi-Dakolo-Iyawo-Mi_tooXclusive.com_.mp3',
+        musicName: 'Iyawo Mi',
+        artistPix: 'timi.jpg'
+    },
+
+    {
+        artistName: 'chike',
+        musicFile: 'Chike-Amen-(JustNaija.com).mp3',
+        musicName: 'Amen',
+        artistPix: 'chike.jpg'
+    },
+
+    {
+        artistName: 'micheal Jackson',
+        musicFile: 'Michael_Jackson_-_You_Are_Not_Alone_Qoret.com.mp3',
+        musicName: 'You and I alone',
+        artistPix: 'jackson.jpg'
+    },
+
+    {
+        artistName: '2face',
+        musicFile: '2Face_Idibia_-_True_Love_Qoret.com.mp3',
+        musicName: 'True Love',
+        artistPix: '2face.jpg'
+    },
+
+    {
+        artistName: 'wande Coal',
+        musicFile: ' Superwoman.mp3',
+        musicName: 'Superwoman',
+        artistPix: 'wande.jpg'
+    },
+
+    {
+        artistName: 'yemi Alade',
+        musicFile: ' Yemi-Alade-Kissing_tooXclusive.com_.mp3',
+        musicName: 'Kissing',
+        artistPix: 'yemi.jpg'
+    },
+
+    {
+        artistName: 'patoraking',
+        musicFile: 'My-Woman-My-Everything-_-tooXclusive.mp3',
+        musicName: 'My Woman',
+        artistPix: 'patoranking.jpg'
+    },
+
+   
+
+    {
+        artistName: 'joeboy',
+        musicFile: 'Dj-Neptune-Nobody-ft.-Joeboy-Mr-Eazi.mp3',
+        musicName: 'Nobody',
+        artistPix: 'joeboy.jpg'
+    },
+
+    {
+        artistName: '2face',
+        musicFile: ' Waploaded_2baba_-_African_Queen-1562529854.mp3',
+        musicName: 'African Queen',
+        artistPix: '2face.jpg'
+    },
+
+    {
+        artistName: 'simi',
+        musicFile: ' Simi-Smile-For-Me.mp3',
+        musicName: 'Smile for Me',
+        artistPix: 'simi.jpg'
+    },
+
+    {
+        artistName: 'joeboy',
+        musicFile: 'Joeboy_-_Baby_.mp3',
+        musicName: 'Baby',
+        artistPix: 'joeboy.jpg'
+    },
+
+   
     
 ]
 
@@ -39,6 +134,13 @@ const fixSong = () => {
 }
 fixSong();
 
+const handleMops = () => {
+    playPause.innerHTML = '+' 
+    audio.pause()
+    setTimeout(() => (image.classList.add('zoom')), 3000)
+   image.classList.remove('zoom')
+}
+
 const handlePlay = () => {
     if(playPause.innerHTML === '='){
         playPause.innerHTML = '+' 
@@ -53,6 +155,7 @@ const handlePlay = () => {
 
 const handleNext = () => {
    count++
+   handleMops()
    if (count > musicArray.length - 1) {
        count = 0
    }
@@ -61,6 +164,7 @@ const handleNext = () => {
 
 const handlePrev = () => {
     count--
+    handleMops()
     if (count < 0) {
         count =  musicArray.length - 1
     }
