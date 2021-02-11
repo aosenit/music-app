7ab2533a945ba7506ddb51b2e5c635460d086be0
Adedoyin <creativitytraz22@gmail.com>
2021-02-11 17:07:32 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const musicArray = [
+    {
+        artistName: 'ed Sheeran',
+        musicFile: 'Ed-Sheeran-Perfect-via-Naijafinix.com_.mp3',
+        musicName: 'Perfect',
+        artistPix: 'ed sheeran.jpg'
+    },
+
+    {
+        artistName: 'bruno Mars',
+        musicFile: 'Bruno_Mars_-_Marry_You_Qoret.com.mp3',
+        musicName: 'Marry You',
+        artistPix: 'bruno.jpg'
+    },
+
+    {
+        artistName: 'westlife',
+        musicFile: '/Westlife_-_My_Love.mp3',
+        musicName: 'My Love',
+        artistPix: 'westlife.jpg'
+    }
+    
+]
+
+const image = document.querySelector('.image')
+const audio = document.querySelector('.audioplayer')
+const artistNames = document.querySelector('.artist-name')
+const songTitle = document.querySelector('.song-title')
+const previous = document.querySelector('.prev')
+const playPause = document.querySelector('.play')
+const next = document.querySelector('.next')
+
+let count = 0
+const fixSong = () => {
+    image.src = musicArray[count].artistPix
+    audio.src = musicArray[count].musicFile
+    artistNames.innerHTML = musicArray[count].artistName
+    songTitle.innerHTML = musicArray[count].musicName
+}
+fixSong();
+
+const handlePlay = () => {
+    if(playPause.innerHTML === `<i class='bx bx-play-circle'></i>`){
+        playPause.innerHTML = `<i class='bx bx-pause-circle'></i>'`
+        audio.pause()
+    }
+
+    else if(playPause.innerHTML === `<i class='bx bx-pause-circle'></i>`){
+        playPause.innerHTML = `<i class='bx bx-play-circle'></i>` 
+        audio.play()
+    }
+}
+
+const handleNext = () => {
+   count++
+   if (count > musicArray.length - 1) {
+       count = 0
+   }
+   fixSong()
+}
+
+const handlePrev = () => {
+    count--
+    if (count < 0) {
+        count =  musicArray.length - 1
+    }
+    fixSong()
+ }
+
+playPause.addEventListener('click', handlePlay)
+next.addEventListener('click', handleNext)
+previous.addEventListener('click', handlePrev)
